9daacb27ac0651c696004585d8799e9659734dff
Usuket <sy0202002@gmail.com>
2019-09-12 23:21:27 +0900

    Init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {SimpleHttpServerApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {SimpleHttpServerApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new SimpleHttpServerApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
